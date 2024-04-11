9e1f837522daa5fe9a2c1603c7c3f27eda19166a
santinux <santinofuentes24.sf@gmail.com>
2024-04-11 12:38:27 -0300

    Cola Terminada

---
+++ EstructurasDeDatos/src/lineales/dinamicas/Lista.java
@@ -0,0 +1,15 @@
+package lineales.dinamicas;
+
+/**
+ * Implementación del TDA Nodo
+ * @author santino.fuentes
+ * @version 1.0
+ */
+public class Lista {
+        private Nodo cabecera;
+        
+        public Lista()
+        {
+                this.cabecera = null;
+        }
+}

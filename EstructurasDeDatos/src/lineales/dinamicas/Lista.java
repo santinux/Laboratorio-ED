6567061752b956ab668190fdb08f1c8a0d10c673
santinux <santinofuentes24.sf@gmail.com>
2024-04-12 11:55:56 -0300

    Inicio Lista

--- EstructurasDeDatos/src/lineales/dinamicas/Lista.java
+++ EstructurasDeDatos/src/lineales/dinamicas/Lista.java
@@ -8,8 +8,63 @@ package lineales.dinamicas;
 public class Lista {
         private Nodo cabecera;
         
+        /**
+         * Crea una lista vacía.
+         */
         public Lista()
         {
                 this.cabecera = null;
+        }
+        
+        /**
+         * Agrega un elemento en una posición.
+         * @param unElemento
+         * @param unaPosicion
+         * @return 
+         */
+        public boolean insertar(Object unElemento, int unaPosicion)
+        {
+                //TODO
+        }
+        
+        public boolean eliminar(int unaPosicion)
+        {
+                //TODO
+        }
+        
+        public Object recuperar(int unaPosicion)
+        {
+                //TODO
+        }
+        
+        public int localizar(Object unElemento)
+        {
+                //TODO
+        }
+        
+        public int longitud()
+        {
+                //TODO
+        }
+        
+        public boolean esVacia()
+        {
+                //TODO
+        }
+        
+        public void vaciar()
+        {
+                //TODO
+        }
+        
+        public Lista clonar()
+        {
+                //TODO
+        }
+        
+        @Override
+        public String toString()
+        {
+                //TODO
         }
 }

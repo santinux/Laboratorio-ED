6a9841cb5fcdbdd1e1286027a74298c20848ae1e
santinux <santinofuentes24.sf@gmail.com>
2024-04-18 22:57:49 -0300

    Se agregó: Lista.insertar

--- EstructurasDeDatos/src/lineales/dinamicas/Lista.java
+++ EstructurasDeDatos/src/lineales/dinamicas/Lista.java
@@ -25,19 +25,28 @@ public class Lista {
         public boolean insertar(Object unElemento, int unaPosicion)
         {
                 boolean exito = true;
+                // Verifica que la posición a insertar sea válida
                 if (unaPosicion < 1 || unaPosicion > this.longitud() + 1) {
                         exito = false;
                 } else {
                         if (unaPosicion == 1) {
+                                // Crea el nodo y lo enlaza
                                 this.cabecera = new Nodo(unElemento, this.cabecera);
                         } else {
+                                // Crea un nodo auxiliar para recorrer la lista
                                 Nodo aux = this.cabecera;
                                 int i = 1;
+                                // Recorre hasta la posición anterior a la deseada
                                 while (i < unaPosicion - 1) {
                                         aux = aux.getEnlace();
+                                        i++;
                                 }
+                                // Crea el nodo y lo enlaza
+                                Nodo nuevo = new Nodo(unElemento, aux.getEnlace());
+                                aux.setEnlace(nuevo);
                         }
                 }
+                return (exito);
         }
         
         public boolean eliminar(int unaPosicion)

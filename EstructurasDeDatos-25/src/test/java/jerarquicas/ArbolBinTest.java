b5faa2ebda6079c68f23069a11fc19092ea01623
santinux <santinofuentes24.sf@gmail.com>
2025-04-16 20:15:15 -0300

    Add: Plantilla del test de arbol binario.

---
+++ EstructurasDeDatos-25/src/test/java/jerarquicas/ArbolBinTest.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package jerarquicas;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author sang
+ */
+public class ArbolBinTest
+{
+        
+        public ArbolBinTest()
+        {
+        }
+        
+        @BeforeAll
+        public static void setUpClass()
+        {
+        }
+        
+        @AfterAll
+        public static void tearDownClass()
+        {
+        }
+        
+        @BeforeEach
+        public void setUp()
+        {
+        }
+        
+        @AfterEach
+        public void tearDown()
+        {
+        }
+
+        // TODO add test methods here.
+        // The methods must be annotated with annotation @Test. For example:
+        //
+        // @Test
+        // public void hello() {}
+}
